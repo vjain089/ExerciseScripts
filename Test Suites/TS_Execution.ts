c62d9a20590e1517d2b6ba13a4a55b2b303f4258
vaibhavjain <vaibhavjain@192.168.225.40>
2023-07-11 16:03:16 +0530

    chdngs

--- Test Suites/TS_Execution.ts
+++ Test Suites/TS_Execution.ts
@@ -12,27 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>12081a00-7966-4ed1-bf06-e7014b8cb31c</testSuiteGuid>
    <testCaseLink>
-      <guid>976a428b-09ce-446d-990e-564768e9a787</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Assignment5</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a524f1ec-966a-4d27-8aa6-837d3bf35170</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Assignment4</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>27985dc3-0950-4672-91af-363281cb5309</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Assignment6</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>3fbe583e-9e43-4ff9-ba8e-97cfb1c3303a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
@@ -44,5 +23,26 @@
          <value></value>
          <variableId>a93bc645-200b-4f5c-a407-5c5b5b17b6ed</variableId>
       </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a524f1ec-966a-4d27-8aa6-837d3bf35170</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Assignment4</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>976a428b-09ce-446d-990e-564768e9a787</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Assignment5</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>27985dc3-0950-4672-91af-363281cb5309</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Assignment6</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
